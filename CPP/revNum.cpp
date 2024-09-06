6d373c9e334d1a90377bfd8a84d623ed7d34c362
Augnik03 <shresnik2004@gmail.com>
2024-09-06 17:54:23 +0530

    initial commit

---
+++ CPP/revNum.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int revNum{0}, n, lstdigit;
+    while(n > 0){
+        lstdigit = n % 10;
+        revNum = (revNum*10) + lstdigit;
+        n = n/10;
+    } 
+    cout<<revNum;
+}
