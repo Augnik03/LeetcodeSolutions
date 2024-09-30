dcc57bb9202702701e58c23a531e4baaa348ef1f
Augnik03 <shresnik2004@gmail.com>
2024-10-01 01:13:47 +0530

    changed the code for checking negative numbers as well

--- CPP/revNum.cpp
+++ CPP/revNum.cpp
@@ -1,14 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int revNum{0}, n, lstdigit;
-    cout<<"Enter the number:";
-    cin>>n;
-    while(n > 0){
+int reverse(int n){
+    int revNum{0}, lstdigit;
+    if(n<0){
+        n = -1 * n;
+        while(n > 0){
         lstdigit = n % 10;
         revNum = (revNum*10) + lstdigit;
         n = n/10;
-    } 
-    cout<<revNum;
+        }
+        revNum = -1 * revNum;
+    }
+    else{
+        while(n > 0){
+        lstdigit = n % 10;
+        revNum = (revNum*10) + lstdigit;
+        n = n/10;
+        }
+    }
+    return revNum;
+}
+
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+    cout<<reverse(n);
 }
