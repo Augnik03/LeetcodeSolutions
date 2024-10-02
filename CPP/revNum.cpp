b7434a2a49407de86f047df342c4c25a81bd0d35
Augnik03 <shresnik2004@gmail.com>
2024-10-02 21:03:10 +0530

    solved according to leetcode test cases and submitted on leetcode

--- CPP/revNum.cpp
+++ CPP/revNum.cpp
@@ -1,30 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define INT_MIN  (-INT_MAX-1)
+#define INT_MAX  2147483647
 
-int reverse(int n){
-    int revNum{0}, lstdigit;
-    if(n<0){
-        n = -1 * n;
-        while(n > 0){
+int reverse(long long n)
+{
+    long long revNum{0}, lstdigit;
+    while (abs(n) != 0)
+    {
         lstdigit = n % 10;
-        revNum = (revNum*10) + lstdigit;
-        n = n/10;
+        revNum = (revNum * 10) + lstdigit;
+        if ((revNum < INT_MIN) || (revNum > INT_MAX))
+        {
+            return 0;
         }
-        revNum = -1 * revNum;
-    }
-    else{
-        while(n > 0){
-        lstdigit = n % 10;
-        revNum = (revNum*10) + lstdigit;
-        n = n/10;
-        }
+        n = n / 10;
     }
     return revNum;
 }
 
-int main(){
-    int n;
-    cout<<"Enter the number:";
-    cin>>n;
-    cout<<reverse(n);
+int main()
+{
+    long long n;
+    cout << "Enter the number:";
+    cin >> n;
+    cout << reverse(n);
 }
+
+
+// My solution
+//  int reverse(long long n)
+//  {
+//      long long revNum{0}, lstdigit;
+//      if (n < 0)
+//      {
+//          n = -1 * n;
+//          while (n != 0)
+//          {
+//              lstdigit = n % 10;
+//              revNum = (revNum * 10) + lstdigit;
+//              if (revNum < INT_MIN || revNum > INT_MAX)
+//              {
+//                  return 0;
+//              }
+//              n = n / 10;
+//          }
+//          revNum = -1 * revNum;
+//      }
+//      else
+//      {
+//          while (n > 0)
+//          {
+//              lstdigit = n % 10;
+//              revNum = (revNum * 10) + lstdigit;
+//              if (revNum < INT_MIN || revNum > INT_MAX)
+//              {
+//                  return 0;
+//              }
+//              n = n / 10;
+//          }
+//      }
+//      return revNum;
+//  }
--- CPP/stringPalindrome.cpp
+++ CPP/stringPalindrome.cpp
@@ -3,33 +3,30 @@ using namespace std;
 
 bool isPalindrome(string s){
     if (s.length()<=1){
+        cout<<boolalpha;
         return true;
     }
     int st=0, en=s.length()-1;
     while(st<en){
-        while(st<en && !isalnum(s[st])){
+        if(!isalnum(s[st])){
             st++;
         }
-        while(st<en && isalnum(s[en])){
+        else if(!isalnum(s[en])){
             en--;
         }
-        if(st<en && tolower(s[st]) != tolower(s[en])){
+        if(tolower(s[st++]) != tolower(s[en--])){
+            cout<<boolalpha;
             return false;
         }
-        st++;
-        en--;
     }
+    cout<<boolalpha;
     return true;
 
 }
 
-
-// bool palindrome(int i, string &s, int n){ for array of words, continuous words without space
-//     if(i>=n/2){
-//         return true;
-//     }
-//     if(s[i]!=s[n-i-1]){
-//         return false;
-//     }
-//     return palindrome(i+1,s,n);
-// }
+int main(){
+    string s;
+    cout<<"Enter the string:";
+    cin>>s;
+    cout<<isPalindrome(s);
+}
