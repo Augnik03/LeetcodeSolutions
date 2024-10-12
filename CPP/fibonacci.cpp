b9d34de55152a5b38d617410e2edb30fa6294adc
Augnik03 <shresnik2004@gmail.com>
2024-10-12 22:15:44 +0530

    memorization technique applied in a better way

--- CPP/fibonacci.cpp
+++ CPP/fibonacci.cpp
@@ -1,40 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int fib(int n)
+{
 
-int F[10];
-int fib(int n){
-    
-    if(n<=1){
-        F[n] = n;
+    if (n <= 1)
+    {
         return n;
     }
-    else{
-        if(F[n-2] == -1){
-            F[n-2] = fib(n-2);
-        }
-        if(F[n-1] == -1){
-            F[n-1] = fib(n-1);
-        }
-        return F[n-2]+F[n-1];
+
+    int prev{0}, next{1}, curr;
+    for (int i = 2; i <= n; i++)
+    {
+        curr = prev + next;
+        prev = next;
+        next = curr;
     }
-    
+    return curr;
 }
 
-int main(){
-    for(int i=0; i<10; i++){
-        F[i] = -1;
-    }
+int main()
+{
     int n;
-    cout<<"Enter the number till which you want the fibonacci series for:";
-    cin>>n;
-    cout<<"Fibonacci series is:"<<fib(n);
+    cout << "Enter the number till which you want the fibonacci series for:";
+    cin >> n;
+    cout << "Fibonacci series is:" << fib(n);
 }
-
-
 
 /*
-Leetcode Solution:
+My Solution:
 
 int fib(int n) {
         int F[n+1];
@@ -49,7 +43,7 @@ int fib(int n) {
             if(F[n-2] == -1){
                 F[n-2] = fib(n-2);
             }
-            
+
             if(F[n-1] == -1){
                 F[n-1] = fib(n-1);
             }
